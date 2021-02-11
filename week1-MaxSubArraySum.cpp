118c7c549d42e7ac4922c5b91133126554054b7f
xuyiqun-learner <xuzhimomemeda@gmail.com>
2021-02-11 22:41:59 +0800

    feat(week1-maxsubarraysum.cpp): add new practice

--- week1-MaxSubArraySum.cpp
+++ week1-MaxSubArraySum.cpp
@@ -2,34 +2,32 @@
 #include <stdlib.h>
 
 int MaxSubArraySum(int size, int integer[]) {
-//     int a[size];
     int tempsum = 0;
-//     int sum[size] = 0;
-    int maxsum = integer[0];
-    for(int i=0,j=0; i < size-1; i++) {
+    int maxsum = 0;
+//     printf("initial maxsum is %d\n", maxsum);
+    for(int i=1; i < size-1; i++) {
         tempsum += integer[i];
         if(tempsum > maxsum) {
             maxsum = tempsum;
         }
-        if(tempsum < 0) {
+        else if(tempsum < 0) {
             tempsum = integer[i];
+//             printf("new tempsum is %d\n", tempsum);
         }
-  
     }
-    if(tempsum < 0) {
-        return 0;
-    }
+
     return maxsum;
 }
 
 
 int main() {
     int size = 0;
-    int a[100000];
-    memset(a, 0, 100000);
+    int a[100000] = {0};
+//     memset(a, 0, 100000);
     scanf("%d",&size);
     for(int i=0; i<size; i++) {
         scanf("%d",&a[i]);
     }
-    MaxSubArraySum(size, a);
+    printf("%d",MaxSubArraySum(size, a));
+    return 0;
 }
